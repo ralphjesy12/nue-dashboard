2aec2528373b3c89cad58e43c75cb5d87ec7b96d
Ralph John Galindo <ralphjesy@gmail.com>
2016-08-21 10:16:28 +0800

    First Commit
    * Plugin Description

---
+++ nue-dashboard.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Nue Dashboard
+Plugin URI: https://github.com/ralphjesy12/nue-dashboard
+Description: A Wordpress Plugin for an innovated WP Admin UI
+Version: 1.0.0
+Author: Ralph John Galindo
+Author URI: https://github.com/ralphjesy12
+License: GPLv2 or later
+*/
